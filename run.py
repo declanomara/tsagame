dcca27997a2133941df395825beea9cfdecb31b2
declan <25646795+declanomara@users.noreply.github.com>
2019-03-18 22:59:17 -0400

    Added method to get path to resources based on whether you are running from source or dist

--- run.py
+++ run.py
@@ -1,9 +1,21 @@
-import pygame
 import datetime
 import sys
+import os
 from lib.managers import *
 from levels.levels import *
 from pygame.locals import *
+
+
+if getattr(sys, 'frozen', False):
+    print('Running from .exe, not source')
+else:
+    print('Running from source, using resources/')
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+    return os.path.join(base_path, relative_path)
+
 
 if __name__ == '__main__':
     # TODO: replace FPS and RES with some sort of options manager
