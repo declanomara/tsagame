9e6b5cebfb673333884a4567620eaecf925bc32d
declan <25646795+declanomara@users.noreply.github.com>
2019-03-18 21:06:34 -0400

    Changed player to render with pixel height

--- levels/objects/player.py
+++ levels/objects/player.py
@@ -8,18 +8,15 @@ class Player:
         self.texture = pygame.image.load(texture)
         self.rect = self.texture.get_rect()
         self.base_movement_speed = {'x': 1, 'y': 1}
-        self.scale = 20
+        self.scale = 64
 
     def move(self, direction, timedelta):
         self.x += self.base_movement_speed['x'] * direction['x'] * timedelta * 1000
         self.y += self.base_movement_speed['y'] * direction['y'] * timedelta * 1000
-        print(self.base_movement_speed['y'] * direction['y'] * timedelta * 1000, direction['y'])
 
     def draw(self, surface):
-        scale = self.scale
-        x, y = surface.get_size()
-        width = int(x/scale)
-        height = width*2
+        width = int(self.scale/2)
+        height = int(self.scale)
         rect = pygame.Rect(self.x, self.y, width, height)
         t = pygame.transform.scale(self.texture, (width, height))
         surface.blit(t, rect)
